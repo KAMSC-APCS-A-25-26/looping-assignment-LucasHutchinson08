ec742428dba72e929878da94c9c6f8976d565fc7
LucasHutchinson08 <03lucas.hutchinson@kamsc.org>
2025-10-19 21:38:58 -0400

    first attempt

--- src/main/java/SumOfEvenNumbers.java
+++ src/main/java/SumOfEvenNumbers.java
@@ -2,6 +2,19 @@ import java.util.Scanner;
 
 public class SumOfEvenNumbers {
     public static void main(String[] args) {     
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter an integer value: ");
+        int integer = sc.nextInt();
+
+        if (integer < 2) {
+            System.out.println("Error - Integer must be greater than 2");
+        }
+        int sum = 0;
+        for (int i = 2; i <= integer; i += 2) {
+            sum +=i;
+        }
+        System.out.println("The sum of even numbers between 2 and " + integer + " is " + sum +".");
         // TODO: Step 1 - Get user input
 
         // TODO: Step 2 - Validate input
