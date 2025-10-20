989bc24898d9c2ae992c733aa75a0f08cd9e7b55
LucasHutchinson08 <03lucas.hutchinson@kamsc.org>
2025-10-19 21:47:05 -0400

    3rd

--- src/main/java/SumOfEvenNumbers.java
+++ src/main/java/SumOfEvenNumbers.java
@@ -8,7 +8,7 @@ public class SumOfEvenNumbers {
         int integer = sc.nextInt();
 
         if (integer < 2) {
-            System.out.println("Error - Integer must be greater than 2");
+            System.out.println("Error: Input must be 2 or greater.");
             sc.close();
             return;
         }
@@ -17,13 +17,6 @@ public class SumOfEvenNumbers {
             sum +=i;
         }
         System.out.println("The sum of even numbers between 2 and " + integer + " is " + sum +".");
-        // TODO: Step 1 - Get user input
-
-        // TODO: Step 2 - Validate input
-        
-        // TODO: Step 3 - Calculate sum of even numbers
-        
-        // TODO: Step 4 - Display the result
 
     }
 }
